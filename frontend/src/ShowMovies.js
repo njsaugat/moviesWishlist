462456c450d80e3ff27e1eca6dec4a8b03174d0f
Saugat Poudel <njsaugat@gmail.com>
2022-11-27 11:45:58 +0545

    show movies file added

--- frontend/src/ShowMovies.js
+++ frontend/src/ShowMovies.js
@@ -4,13 +4,14 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link, useLocation } from 'react-router-dom';
 import GetMovie from './GetMovie';
+import Landing from './landingPage/LandingPage';
 
 const searchIcon = <FontAwesomeIcon icon={faMagnifyingGlass} />;
 // function ShowSearchResults({ searchTerm }) {
 //   return (
-//     <div className="search-results">
+//     <div className="search-results text-gray-500 ">
 //       {/* {`Search Results for`}  */}
-//       <span>"{searchTerm.toUpperCase()}" </span>
+//       <span className="text-black tracking-wide">"{searchTerm.toUpperCase()}" </span>
 //     </div>
 //   );
 // }
@@ -25,12 +26,19 @@ export default function ShowMovies() {
     })();
   }, [searchTerm]);
   if (movies === null || movies.length === 0) {
-    return <h1>Loading...</h1>;
+    return (
+      <h1 className="font-bold flex items-center justify-center text-4xl mt-4">
+        Loading...
+      </h1>
+    );
   }
-
+  console.log(movies);
   return (
-    <div className="container">
-      <div className="search-bar">
+    <div className=" w-screen h-screen m-0 flex items-center justify-center flex-col  bg-gradient-to-tr from-gray-600 to-indigo-700">
+      <div
+        className="search-bar 
+       "
+      >
         <input
           type="text"
           placeholder="Search Movies..."
@@ -45,13 +53,11 @@ export default function ShowMovies() {
         />
         <span className="search">{searchIcon}</span>
       </div>
-      {/* <ShowSearchResults searchTerm={'popular movies'} /> */}
-      <div className="movies">
+      <div className="movies w-screen  flex items-center justify-center gap-7 flex-wrap  md:w-full h-full  relative m-0 px-48">
         {movies.map((movie) => {
           return (
             <>
               <Link to={`/movie/${movie.id}`} key={movie.id} state={movie}>
-                {/* have to pass in the above place */}
                 <GetMovie movie={movie} />
               </Link>
               {movieLink.state}
@@ -59,6 +65,8 @@ export default function ShowMovies() {
           );
         })}
       </div>
+      {/* hello! */}
     </div>
+    // </div>
   );
 }
