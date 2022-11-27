b960ebd1b993e8978934004320b3513b8f44a118
Saugat Poudel <njsaugat@gmail.com>
2022-11-27 11:43:36 +0545

    get movie file added

--- frontend/src/GetMovie.js
+++ frontend/src/GetMovie.js
@@ -2,12 +2,19 @@ const IMAGE_URL = 'https://image.tmdb.org/t/p/original/';
 const yearCount = 4;
 export default function GetMovie({ movie }) {
   return (
-    <div className="movie">
-      <img src={IMAGE_URL + movie.backdrop_path} alt="" srcSet="" />
+    <div className="movie w-11/12 md:w-full   cursor-pointer relative flex items-center justify-center rounded-lg ">
+      <img
+        src={IMAGE_URL + movie.backdrop_path}
+        alt=""
+        srcSet=""
+        className="h-full md:h-72  object-cover mb-0  w-full lg:w-96 "
+      />
 
-      <div className="details">
-        <div className="left">
-          <span className="name">{movie.title}</span>
+      <div className="details flex items-center justify-center ">
+        <div className="left flex  justify-center flex-col items-start">
+          <span className="name mb-3 font-bold tracking-wider">
+            {movie.title}
+          </span>
           <span className="likes">
             {movie.release_date.substr(0, yearCount)}{' '}
           </span>
