0e86c867695f1f01dc1cf191fbf361da14db5b5b
Saugat Poudel <njsaugat@gmail.com>
2022-11-30 10:49:58 +0545

    heroes file added

--- frontend/src/landingPage/Heroes.js
+++ frontend/src/landingPage/Heroes.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Tagline from '../components/Tagline';
+import Waves from './Waves';
+import WhiteScreen from './WhiteScreen';
 
 const Heroes = () => {
+  const [up, moveUp] = useState(false);
+  useEffect(() => {
+    let interval;
+    function playAnimation() {
+      up === false ? moveUp(true) : moveUp(false);
+    }
+    interval = setInterval(playAnimation, 1000);
+    return () => clearInterval(interval);
+  }, [up]);
   return (
-    <div className="bg-gradient-to-r from-slate-700 to-slate-900 w-screen min-h-screen ">
-      <div className="custom-shape-divider-bottom-1669704609  ">
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
+    <>
+      <main className="w-screen p-10 md:p-20 relative md:flex md:justify-between items-center overflow-hidden   mb-32 bg-gradient-to-r from-slate-700 to-slate-900">
+        <div className="left my-24 mb-10  md:w-3/5">
+          <Tagline />
+          <section className="description text-gray-400 md:w-1/2">
+            Our team of experts uses a methodology to identify the credit cards
+            most likely to fit your needs. We examine annual percentage rates,
+            annual fees.
+          </section>
+          <Link to="/explore">
+            <button className="py-3 px-5 my-10 bg-gradient-to-t from-purple-500 to-purple-100  rounded-lg text-black hover:shadow-xl hover:scale-105 transition-all duration-300">
+              Explore Now
+            </button>
+          </Link>
+        </div>
+        <div
+          className={`${
+            up ? 'translate-y-5' : 'translate-y-0'
+          } md:w-1/2   transition-all duration-1000 `}
         >
-          <path
-            fill="#fff"
-            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            class="shape-fill"
-          ></path>
-        </svg>
-      </div>
-    </div>
+          <WhiteScreen />
+        </div>
+        <Waves />
+      </main>
+      {/* <div>hello</div> */}
+    </>
   );
 };
 
