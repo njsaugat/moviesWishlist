323600aba7406de2694c36064c39c5e9227d10e2
Saugat Poudel <njsaugat@gmail.com>
2022-11-30 10:53:00 +0545

    white screen file added

---
+++ frontend/src/landingPage/WhiteScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const WhiteScreen = () => {
+  return (
+    <div className="contaienr mb-36">
+      <div className="tilt-box-wrap rounded-lg">
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <span className="t_over"></span>
+        <div className="tilt-box">
+          <strong>{/* Tilt <br /> Effect */}</strong>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhiteScreen;
