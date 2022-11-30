ff669c69bccc72dc725e4a00b991c4728e9bcc30
Saugat Poudel <njsaugat@gmail.com>
2022-11-30 10:52:18 +0545

    landing page file added

--- frontend/src/landingPage/LandingPage.js
+++ frontend/src/landingPage/LandingPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Business from './Business';
+import Footer from './Footer';
 import Hero from './Hero';
 import Heroes from './Heroes';
+import TryService from './TryService';
 
 const LandingPage = () => {
   return (
     <>
       <Heroes />
+      <Business />
+      <TryService />
+      <Footer />
       {/* <Hero />
        */}
     </>
