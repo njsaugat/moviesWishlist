95dbc77f49445c5a7db3cb6f3147061d64b48a6f
Saugat Poudel <njsaugat@gmail.com>
2022-12-02 09:07:04 +0545

    app js file added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './landingPage/LandingPage';
 import Landing from './landingPage/LandingPage';
+import Home from './movies/Home';
 import ShowMovie from './ShowMovie';
 import ShowMovies from './ShowMovies';
 import './style.css';
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />;
         {/* <Route path="/" element={<ShowMovies />} />; */}
+        <Route path="/home" element={<Home />} />;
         <Route path="/movie/:id" element={<ShowMovie />} />;
       </Routes>
     </BrowserRouter>
