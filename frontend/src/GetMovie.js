e6c6b3138611e5eddd4cf03a8a749d31179b0d2d
Saugat Poudel <njsaugat@gmail.com>
2022-12-03 11:26:31 +0545

    get movie file added

--- frontend/src/GetMovie.js
+++ frontend/src/GetMovie.js
@@ -2,7 +2,8 @@ const IMAGE_URL = 'https://image.tmdb.org/t/p/original/';
 const yearCount = 4;
 export default function GetMovie({ movie }) {
   return (
-    <div className="movie w-11/12 md:w-full   cursor-pointer relative flex items-center justify-center rounded-lg ">
+    // <div className="movie w-11/12 md:w-full   cursor-pointer relative flex items-center justify-center rounded-lg ">
+    <div className="movie w-full  h-full cursor-pointer relative flex items-center justify-center rounded-lg ">
       <img
         src={IMAGE_URL + movie.backdrop_path}
         alt=""
@@ -13,7 +14,7 @@ export default function GetMovie({ movie }) {
       <div className="details flex items-center justify-center ">
         <div className="left flex  justify-center flex-col items-start">
           <span className="name mb-3 font-bold tracking-wider">
-            {movie.title}
+            {/* {movie.title} */}
           </span>
           <span className="likes">
             {movie.release_date.substr(0, yearCount)}{' '}
