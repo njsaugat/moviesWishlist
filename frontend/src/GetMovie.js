1f6264f7ef9c014b794e88ec87d9a68bf773b329
Saugat Poudel <njsaugat@gmail.com>
2022-12-05 16:27:59 +0545

    get movie file added

--- frontend/src/GetMovie.js
+++ frontend/src/GetMovie.js
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const IMAGE_URL = 'https://image.tmdb.org/t/p/original/';
 const yearCount = 4;
 export default function GetMovie({ movie }) {
   return (
     // <div className="movie w-11/12 md:w-full   cursor-pointer relative flex items-center justify-center rounded-lg ">
-    <div className="movie w-full  h-full cursor-pointer relative flex items-center justify-center rounded-lg ">
+    <Link
+      className="movie w-full  h-full cursor-pointer relative flex items-center justify-center rounded-lg "
+      to={`/movie/${movie.id}`}
+      state={movie}
+    >
       <img
         // src={IMAGE_URL + movie.backdrop_path}
         src={IMAGE_URL + movie.poster_path}
@@ -22,16 +28,8 @@ export default function GetMovie({ movie }) {
             {/* {movie.release_date.substr(0, yearCount)}{' '} */}
           </span>
         </div>
-        <div className="right">
-          {/* <a
-                  href={movie.user.social.portfolio_url}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src={movie.user.profile_image.large} alt="" srcSet="" />
-                </a> */}
-        </div>
+        <div className="right"></div>
       </div>
-    </div>
+    </Link>
   );
 }
