3226f0858e6a5fafddbe0c95bef81dc584fda4f3
Saugat Poudel <njsaugat@gmail.com>
2022-12-07 11:13:02 +0545

    show movie file added

--- frontend/src/movie/ShowMovie.js
+++ frontend/src/movie/ShowMovie.js
@@ -1,17 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import MovieBg from '../components/MovieBg';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import {
+  faStar,
+  faAngleDown,
+  faArrowUpRightFromSquare,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Navbar from '../components/Navbar';
 import { getMovie } from '../movies/getMovies';
-import { getCast, getSimilarMovies } from './getCast';
+import {
+  getCast,
+  getImages,
+  getRecommendedMovies,
+  getReviews,
+  getSimilarMovies,
+  getVideos,
+} from './getCast';
 import ScrollMovie from '../components/ScrollMovie';
+import YoutubeEmbed from '../components/YoutubeEmbed';
+import NameGenerator from '../components/NameGenerator';
+import Reviews from './Reviews';
+import Footer from '../components/Footer';
 
+const showMoreIcon = <FontAwesomeIcon icon={faAngleDown} />;
+const shareIcon = <FontAwesomeIcon icon={faArrowUpRightFromSquare} />;
 const IMAGE_URL = 'https://image.tmdb.org/t/p/original/';
-
 const starIcon = <FontAwesomeIcon icon={faStar} />;
-
+const TOTAL_TRAILER = 1;
 export default function ShowMovie() {
   // const movieLink = useLocation();
   // const movie = movieLink.state;
@@ -20,14 +36,27 @@ export default function ShowMovie() {
   const [loading, setLoading] = useState(true);
   const [cast, setCast] = useState([{}]);
   const [similarMovies, setSimilarMovies] = useState([{}]);
-  console.log(similarMovies);
+  const [recommendedMovies, setRecommendedMovies] = useState([{}]);
+  const [reviews, setReviews] = useState([{}]);
+  const [images, setImages] = useState({});
+  const [videos, setVideos] = useState([{}]);
+  const [showItems, setShowItems] = useState(1);
+  const [showMore, setShowMore] = useState(false);
+
   const params = useParams();
+  let trailerCount = 0;
+
   useEffect(() => {
     (async () => {
       setMovie(await getMovie(params.id));
       setCast(await getCast(params.id));
-      setSimilarMovies(await getSimilarMovies(params.id));
+      setVideos(await getVideos(params.id));
+
+      setReviews(await getReviews(params.id));
       setLoading(false);
+      setSimilarMovies(await getSimilarMovies(params.id));
+      setRecommendedMovies(await getRecommendedMovies(params.id));
+      setImages(await getImages(params.id));
     })();
   }, [params.id]);
   // return (
@@ -118,7 +147,8 @@ export default function ShowMovie() {
           </div>
         </div>
       </div>
-      <div className="w-screen h-screen pl-5 md:pl-10 lg:pl-16 py-36 bg-gradient-to-t from-slate-700 to-slate-900 md:py-0">
+
+      <div className="w-screen py-56 pl-5 md:pl-10 lg:pl-36 bg-gradient-to-t from-slate-700 to-slate-900 md:py-0">
         <h1 className="py-5 text-2xl font-bold">Cast</h1>
         <div className="flex items-center w-11/12 overflow-x-auto container-movies">
           <div className="flex items-center justify-center gap-5 flex-nowrap">
@@ -139,8 +169,88 @@ export default function ShowMovie() {
             })}
           </div>
         </div>
+
+        {videos.map((video) => {
+          // console.log(trailerCount);
+          if (
+            video.type.toLowerCase() === 'trailer' &&
+            trailerCount < TOTAL_TRAILER
+          ) {
+            trailerCount++;
+            return <YoutubeEmbed videoId={video.key} />;
+          } else {
+            return '';
+          }
+        })}
+        {console.log(reviews)}
+        {reviews.length > 0 && (
+          <div className="relative py-5 transition-all duration-300 lg:mb-5">
+            <h1 className="mb-5 text-2xl font-bold ">Reviews</h1>
+
+            <Reviews reviews={reviews} showItems={showItems} />
+            <div
+              className={`${
+                !showMore && 'active'
+              } w-11/12 absolute -bottom-3 lg:-bottom-8  show-more-reviews cursor-pointer flex justify-center items-center h-14  mt-5 `}
+              onClick={(e) => {
+                if (!showMore) {
+                  setShowItems(reviews.length);
+                  setShowMore(true);
+                } else {
+                  setShowItems(1);
+                  setShowMore(false);
+                }
+              }}
+            >
+              <div
+                className={`flex items-center justify-center w-10 h-10 rounded-full shadow-2xl opacity-100 show-more bg-gradient-to-t from-slate-400 to-slate-600`}
+              >
+                <span className={showMore && 'rotate-180'}>{showMoreIcon}</span>
+              </div>
+            </div>
+          </div>
+        )}
+        {!showMore && (
+          <div className="flex flex-col items-center justify-center w-11/12 p-0 m-0">
+            <h1 className="mt-10 mb-5 text-3xl font-bold text-center lg:mt-20 ">
+              Don't wanna
+              <span className="mx-2 text-transparent drop-shadow-md bg-clip-text bg-gradient-to-r from-slate-200 to-slate-400">
+                READ
+              </span>
+              reviews?
+              <span className="mx-2 text-transparent bg-clip-text bg-gradient-to-r from-purple-200 to-purple-500">
+                WATCH
+              </span>
+              'EM{' '}
+            </h1>
+            <a
+              href={`https://www.youtube.com/results?search_query=${movie.title}+review`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="px-5 py-3 my-10 text-black transition-all duration-300 rounded-lg bg-gradient-to-t from-purple-500 to-purple-100 hover:shadow-xl hover:scale-105 animate-pulse ">
+                Watch Now {shareIcon}
+              </button>
+            </a>
+          </div>
+        )}
+        <h1 className="text-2xl font-bold translate-y-8 ">Similar Movies</h1>
         {similarMovies && <ScrollMovie movies={similarMovies} />}
+        <h1 className="mt-10 text-2xl font-bold translate-y-8 ">For YOU</h1>
+        {recommendedMovies && <ScrollMovie movies={recommendedMovies} />}
+        {images &&
+          images.backdrops &&
+          images.backdrops.map((image, index) => {
+            return (
+              <div key={image.file_path}>
+                <img src={IMAGE_URL + image.file_path} alt="" srcset="" />
+              </div>
+            );
+          })}
+        {console.log(images)}
+        {console.log(videos)}
       </div>
+      <Footer />
     </>
   );
 }
