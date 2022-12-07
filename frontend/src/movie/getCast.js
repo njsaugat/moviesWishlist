3512d28f2e7018226131c64b77f8c37131917cfd
Saugat Poudel <njsaugat@gmail.com>
2022-12-07 11:12:34 +0545

    get cast file added

--- frontend/src/movie/getCast.js
+++ frontend/src/movie/getCast.js
@@ -3,7 +3,7 @@ export async function getCast(id) {
   const API_URL = `https://api.themoviedb.org/3/movie/${id}/credits?${api_key}&language=en-US`;
   const resultData = await fetch(API_URL, { accept: 'application/json' });
   const results = await resultData.json();
-  console.log(results);
+  // console.log(results);
   return results.cast;
 }
 
@@ -11,6 +11,36 @@ export async function getSimilarMovies(id) {
   const API_URL = `https://api.themoviedb.org/3/movie/${id}/similar?${api_key}&language=en-US&page=1`;
   const resultData = await fetch(API_URL, { accept: 'application/json' });
   const { results } = await resultData.json();
+  // console.log(results);
+  return results;
+}
+export async function getRecommendedMovies(id) {
+  const API_URL = `https://api.themoviedb.org/3/movie/${id}/recommendations?${api_key}&language=en-US&page=1`;
+  const resultData = await fetch(API_URL, { accept: 'application/json' });
+  const { results } = await resultData.json();
+  // console.log(results);
+  return results;
+}
+export async function getReviews(id) {
+  const API_URL = `https://api.themoviedb.org/3/movie/${id}/reviews?${api_key}&language=en-US&page=1`;
+  const resultData = await fetch(API_URL, { accept: 'application/json' });
+  const { results } = await resultData.json();
+  // console.log(results);
+  return results;
+}
+
+export async function getImages(id) {
+  const API_URL = `https://api.themoviedb.org/3/movie/${id}/images?${api_key}&language=en-US`;
+  const resultData = await fetch(API_URL, { accept: 'application/json' });
+  const results = await resultData.json();
   console.log(results);
-  return results.cast;
+  return results;
+}
+
+export async function getVideos(id) {
+  const API_URL = `https://api.themoviedb.org/3/movie/${id}/videos?${api_key}&language=en-US`;
+  const resultData = await fetch(API_URL, { accept: 'application/json' });
+  const { results } = await resultData.json();
+  // console.log(results);
+  return results;
 }
