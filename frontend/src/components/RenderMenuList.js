15dd44f3591e69813966492a76abd69dc351eef3
Saugat Poudel <njsaugat@gmail.com>
2022-12-08 08:44:13 +0545

    render menu list added

--- frontend/src/components/RenderMenuList.js
+++ frontend/src/components/RenderMenuList.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { LoggedInContext } from '../App';
+// import ShowPortal from '../components/ShowPortal';
 
 const RenderMenuList = ({ icons }) => {
+  const [showOptions, setShowOptions] = useState(false);
+  const { loggedIn } = useContext(LoggedInContext);
+
+  if (!loggedIn) {
+    return (
+      <div className="flex flex-col text-xl lg:w-full lg:ml-40 lg:flex-row gap-y-10 lg:gap-y-0 lg:gap-x-10 lg:text-lg">
+        <Link to={'/home'} rel="noreferrer" className="list-none">
+          {icons && icons[3]} Explore
+        </Link>
+        <Link to={'/login'} rel="noreferrer" className="list-none">
+          {icons && icons[1]} Log In
+        </Link>
+        <Link to={'/signup'} rel="noreferrer" className="list-none">
+          {icons && icons[2]} Sign Up
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <>
       <Link to={'/user/properties'} rel="noreferrer" className="list-none">
@@ -16,6 +37,25 @@ const RenderMenuList = ({ icons }) => {
       <a href="# " rel="noreferrer" className="list-none">
         {icons && icons[1]} About Us
       </a>
+      <div
+        className="list-none cursor-pointer"
+        onClick={(e) => {
+          setShowOptions(true);
+        }}
+      >
+        {icons && icons[1]} Log Out
+      </div>
+      {/* {showOptions && (
+        <ShowPortal
+          showOptions={showOptions}
+          // closeModal={() => setShowOptions(false)}
+          setShowOptions={setShowOptions}
+          // setOverflow={setOverflow}
+
+          message={'Do you want to log out?'}
+          logOut={true}
+        />
+      )} */}
     </>
   );
 };
