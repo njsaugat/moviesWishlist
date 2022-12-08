378c33dd6a60315b66f7a6de343db9c5b483790b
Saugat Poudel <njsaugat@gmail.com>
2022-12-08 08:44:29 +0545

    heroes file updated

--- frontend/src/landingPage/Heroes.js
+++ frontend/src/landingPage/Heroes.js
@@ -16,16 +16,16 @@ const Heroes = () => {
   }, [up]);
   return (
     <>
-      <main className="w-screen p-10 md:p-20 relative md:flex md:justify-between items-center overflow-hidden   mb-32 bg-gradient-to-r from-slate-700 to-slate-900">
-        <div className="left my-24 mb-10  md:w-3/5">
+      <main className="relative items-center w-screen p-10 mb-32 overflow-hidden md:p-20 md:flex md:justify-between bg-gradient-to-r from-slate-700 to-slate-900">
+        <div className="my-24 mb-10 left md:w-3/5">
           <Tagline />
-          <section className="description text-gray-400 md:w-1/2">
+          <section className="text-gray-400 description md:w-1/2">
             Our team of experts uses a methodology to identify the credit cards
             most likely to fit your needs. We examine annual percentage rates,
             annual fees.
           </section>
-          <Link to="/explore">
-            <button className="py-3 px-5 my-10 bg-gradient-to-t from-purple-500 to-purple-100  rounded-lg text-black hover:shadow-xl hover:scale-105 transition-all duration-300">
+          <Link to="/home">
+            <button className="px-5 py-3 my-10 text-black transition-all duration-300 rounded-lg bg-gradient-to-t from-purple-500 to-purple-100 hover:shadow-xl hover:scale-105">
               Explore Now
             </button>
           </Link>
