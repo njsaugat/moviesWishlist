108c4dce8845efa6e2bae70496906c4d4046c7c0
Saugat Poudel <njsaugat@gmail.com>
2022-12-08 08:42:56 +0545

    movies genres file added

---
+++ frontend/src/MoviesGenres.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import DisplayMovies from './movies/DisplayMovies';
+import getMovies, { getMoviesOfGenres } from './movies/getMovies';
+
+const MoviesGenres = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { id, genreName } = useParams();
+
+  // const [searchTerm, setSearchTerm] = useState('');
+  // const movieLink = useLocation();
+  useEffect(() => {
+    (async () => {
+      setMovies(await getMoviesOfGenres(id));
+      setLoading(false);
+    })();
+  }, [id]);
+  console.log(movies);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center w-screen h-screen bg-gradient-to-tr from-slate-400 to-slate-700 ">
+        Loading
+      </div>
+    );
+  }
+  return (
+    <>
+      <DisplayMovies movies={movies} searchTerm={`${genreName} Movies`} />
+    </>
+  );
+};
+
+export default MoviesGenres;
