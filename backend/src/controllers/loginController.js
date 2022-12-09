cf8bd639cd8514dd1ce87bc4a336e7e8fdf9f344
Saugat Poudel <njsaugat@gmail.com>
2022-12-09 11:16:33 +0545

    login added

---
+++ backend/src/controllers/loginController.js
@@ -0,0 +1,39 @@
+const prisma = require('../utils/prismaClient');
+const bcrypt = require('bcrypt');
+exports.postLoginInfo = async (req, res) => {
+  console.log(req.body.body);
+  const { email, password } = req.body.body;
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  console.log(user);
+  if (passwordMatch) {
+    req.session.user = user;
+    req.session.isLoggedIn = true;
+    console.log(req.session);
+    res.send({ loggedIn: true });
+    return req.session.save((err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  } else {
+    res.send({ loggedIn: false });
+  }
+};
+
+exports.logout = (req, res) => {
+  console.log(req.session);
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.status(400).send('Unable to log out');
+      } else {
+        res.send('Logout successful');
+      }
+    });
+  }
+};
