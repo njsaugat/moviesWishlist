ea1eb973545bcb0f4e1f9bdc6c0bd450835420c2
Saugat Poudel <njsaugat@gmail.com>
2022-12-10 06:12:50 +0545

    route file added

--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const { isAuthenticated } = require('../controllers/authController');
 const { postLoginInfo, logout } = require('../controllers/loginController');
+const { addMovie } = require('../controllers/movieController');
 const { postUser, getUser } = require('../controllers/signupController');
 const router = express.Router();
 
 router.post('/user', postUser);
 
 router.post('/login', postLoginInfo);
+
+router.post('/add-movie', addMovie);
 
 router.get('/user/:id', getUser);
 
