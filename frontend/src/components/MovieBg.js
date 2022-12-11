748e87f9fe3c400ec141c110d178fbdd4551751c
Saugat Poudel <njsaugat@gmail.com>
2022-12-11 14:27:11 +0545

    movie bg file added

--- frontend/src/components/MovieBg.js
+++ frontend/src/components/MovieBg.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import GetMovie from '../GetMovie';
 const IMAGE_URL = 'https://image.tmdb.org/t/p/original/';
 const DESCRIPTION_MAX_LENGTH = 100;
 const MovieBg = ({ movie, index, picturePointer, ShowIcons }) => {
@@ -12,14 +13,15 @@ const MovieBg = ({ movie, index, picturePointer, ShowIcons }) => {
       }}
     >
       <div className="flex justify-between w-screen ">
-        <div>
-          <img
+        <div className="absolute object-cover mb-4 -translate-x-1/2 -translate-y-1/2 rounded w-44 h-80 left-1/2 top-2/3 md:left-1/4 md:-translate-x-3/4 md:top-1/4 md:translate-y-3/4">
+          {/* <img
             src={IMAGE_URL + movie.poster_path}
             alt=""
             srcSet=""
             className="absolute object-cover mb-4 -translate-x-1/2 -translate-y-1/2 rounded w-44 h-80 left-1/2 top-2/3 md:left-1/4 md:-translate-x-3/4 md:top-1/4 md:translate-y-3/4"
             lazy="loading"
-          />
+          /> */}
+          <GetMovie movie={movie} />
         </div>
         <div className="absolute flex flex-col flex-wrap w-full text-white bottom-10 md:w-96 lg:w-auto lg:break-words md:left-1/3 md:bottom-1/3 md:translate-y-1/2 md:justify-start md:px-10 ">
           <span className="text-3xl font-bold leading-10 tracking-wider md:text-5xl">
