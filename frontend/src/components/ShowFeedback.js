5af746e6974cbed11ac417fa021da215ab5fd34e
Saugat Poudel <njsaugat@gmail.com>
2022-12-13 10:33:01 +0545

    show feedback added

---
+++ frontend/src/components/ShowFeedback.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+const ShowFeedback = ({ movieFeedback, setMovieFeedback, sendEmail }) => {
+  const [showDiv, setShowDiv] = useState(movieFeedback ? true : false);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowDiv(false);
+      //have set the movie addition feedback to false
+      setMovieFeedback(false);
+    }, 3000); // hide the div after 3 seconds
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [setMovieFeedback]);
+
+  return createPortal(
+    showDiv && (
+      <div
+        className={`
+      fixed z-50 flex justify-center w-56 p-3 tracking-wide text-black rounded-lg movie-feedback bottom-5 right-5 bg-slate-200 `}
+      >
+        {sendEmail ? (
+          <>Email was {movieFeedback ? 'sent' : 'not sent'} !</>
+        ) : (
+          <>Movie was {movieFeedback ? 'added' : 'not added'} !</>
+        )}
+      </div>
+    ),
+    document.getElementById('portal3')
+  );
+};
+export default ShowFeedback;
